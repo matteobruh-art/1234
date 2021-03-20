a67a964a1cd5b2045995368a69dc56bd69e4ef45
mat-teo <mat27art@gmail.com>
2021-03-20 20:47:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -188,6 +188,10 @@ message.channel.send("Caricamento in corso...")
     message.channel.send("Caricamento completato!")
 }, timeout=2000)});
 }
+if(message.content == "nice" || message.content == "NICE" || message.content == "Nice"){
+    message.channel.send("noice");
+    message.channel.send("https://tenor.com/view/noice-nice-click-gif-8843762")
+}
 if(message.content == "chi sono io?"){
     var user = message.member;
     message.channel.send("Tu sei" + user.toString());
