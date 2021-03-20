37369857b97fe5f25f0d843de9a95f813991d2aa
mat-teo <mat27art@gmail.com>
2021-03-20 14:52:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -190,7 +190,7 @@ if(message.content == "!conversazione" && message.member.hasPermission("MANAGE_M
 
 if(message.content.startsWith("bot killa")){
     var utentekillato  = message.mentions.members.first();
-    var random = Math.floor(Math.random() / messaggikiller.lenght);
+    var random = Math.floor(messaggikiller.lenght);
     message.channel.send(utentekillato.toString() + messaggikiller[random]);
 }
 });
